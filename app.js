afc6992c4ef301577333a33ac3d0db69264fe513
Victor Tang <victortang1992@gmail.com>
2015-10-25 00:45:28 -0700

    skeleton, pre-app

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-hi
+var app = angular.module('pomodoro', []);
+
+app.factory('time', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}]);//end of service
+
+app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
+    $scope.test = 'Hello world!';
+    $scope.posts = posts.posts;
+
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
+}]);//end of controller
