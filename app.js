46e24b935cbdb8e11abd2732aff11322f4f944fb
Victor Tang <victortang1992@gmail.com>
2015-11-02 16:10:33 -0800

    1. remove parallax 2. initiate angular 3. draw clock

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-var app = angular.module('pomodoro', []);
+(function(){
+
+var app = angular.module('pomodoro',[]);
 
 app.factory('time', [function(){
-  var o = {
-    posts: []
+  var timers = {
+      cooldown: 5,
+      session: 25,
   };
-  return o;
+  return timers;
 }]);//end of service
 
-app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
-    $scope.test = 'Hello world!';
-    $scope.posts = time.posts;
-
-    $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
+app.controller('MainCtrl', ['$scope', 'time', function($scope, time){
+    $scope.timers = time;
+    $scope.addbreak = function() {
+      timers.cooldown += 1;
     };
+    $scope.lowerbreak = function() {
+      timers.cooldown -= 1;
+    };
+    $scope.addsession = function() {
+      timers.session += 1;
+    };
+    $scope.lowersession = function() {
+      timers.session -= 1;
+    };
+    
 }]);//end of controller
+
+    
+})();
